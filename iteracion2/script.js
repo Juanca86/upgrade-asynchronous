b09d45a19257fd3f4f76600b6b17a28fb44e992f
Charly <plmallorca@gmail.com>
2023-01-21 17:24:51 +0100

    arreglo 2.1

--- iteracion2/script.js
+++ iteracion2/script.js
@@ -1,29 +1,27 @@
-// 2.1 Convierte la siguiente promesa para esperar a ejecutar el console usando 
-// async-await.
-const runTimeOut = () => {
+// 2.1 Convierte la siguiente promesa para esperar a ejecutar el console
+//  usando async-await.
+// const runTimeOut = () => {
+//     const promise = new Promise((resolve) => {
+//         setTimeout(function () {
+//             resolve();
+//         }, 2000);
+//     })
+
+//     promise.then(() => {console.log('Time out!')})
+// };
+
+// runTimeOut();
+
+const runTimeOut = async () => {
     const promise = new Promise((resolve) => {
-        setTimeout(function () {
-            resolve();
-        }, 2000);
-    })
-
-    const resultado = async () => {  
-        const respuesta = await promise.then(() => {console.log('Time out!')})}
-        const res =  respuesta.json()
-        console.log(res);
-};
-
+                setTimeout(function () {
+                    resolve();
+                }, 2000);
+            })
+        await promise 
+        console.log('Time out!');
+}
 runTimeOut();
-
-// const buscar = async () => {
-//     try {
-//         const post = await runTimeOut()
-//     console.log(post);
-//     } catch (error) {
-//         console.log(error);
-//     }
-// }
-// buscar()
 
 // 2.2 Convierte la siguiente función con un fetch utilizando async-await. 
 // Recuerda que para usar .fetch() tendrás que probar el ejercicio en el navegador;
@@ -32,5 +30,4 @@ const getCharacters = async () => {
     const res = await respuesta.json()
     console.log(res);
 }
-
 getCharacters();
